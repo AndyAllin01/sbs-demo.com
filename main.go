7367f9afec052942408625950a9bcf4d24c01d10
AndyAllin01 <37802312+AndyAllin01@users.noreply.github.com>
2019-04-26 07:58:11 +0100

    1.4-Updated_notes_and_documentation

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 /*
 - Very basic code model for sbs tech task.
 - Pseudo code and functions to be filled out and tidied.
+    Obviously lint shows many unused types, structs and funcs
 - Refactoring into packages and some functional reusability is required.
 - Have made this partially runnable to show /index homepage with register
     and login routing.
@@ -29,6 +30,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"sync"
 	"text/template"
@@ -41,8 +43,13 @@ import (
 
 type list struct {
 	listName     string     // name of this list
-	listUsers    []string   // slice of users allowed access to this list
+	listUsers    []user     // slice of users allowed access to this list
 	listContents []listToDo // list details
+}
+
+type user struct {
+	name  string
+	email string
 }
 
 //toDo is top-level list linking user (ID) to a list
@@ -96,6 +103,7 @@ FUNCTIONS BELOW HERE TO BE REFACTORED INTO SEPARATE PACKAGE(S)
 ==============================================================
 */
 
+//signup renders signup page
 func signup(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("SU")
 	w.Header().Set("Content-Type", "text/html")
@@ -113,6 +121,7 @@ func signupPost(w http.ResponseWriter, r *http.Request) {
 	//if unique, add to database user table
 }
 
+//login renders login page
 func login(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	err := tpl.ExecuteTemplate(w, "login.gohtml", nil)
@@ -121,24 +130,46 @@ func login(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+//loginPost validates entered login details
 func loginPost(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("LP")
 	http.Redirect(w, r, "/", http.StatusSeeOther) // temporary redirect
 	//check if username and password is valid
 }
 
-//getList receives a validated user id (email address string)
-//and returns that user's list
-func getList(id string) (*listToDo, error) {
-	return nil, nil
-}
-
 //add an element to the list
-func add(li *listToDo) (*listToDo, error) {
+func add(li *list) (*list, error) {
+	msgUsers("Add", li)
 	return li, nil
 }
 
 //delete an element from the list
-func delete(li *listToDo) (*listToDo, error) {
+func delete(li *list) (*list, error) {
+	msgUsers("Delete", li)
 	return li, nil
+}
+
+//delete an element from the list
+func update(li *list) (*list, error) {
+	msgUsers("Update", li)
+	return li, nil
+}
+
+//getList receives a validated user id (email address string)
+//and returns that user's list
+func getList(id *list) (*listToDo, error) {
+	return nil, nil
+}
+
+//messagee users joined to this list when it is amended in any way
+func msgUsers(act string, id *list) {
+	getList(id)
+
+	//message users with interest in the list stating the action (add/del)
+	//performed on which list element e.g. "learn JWT - DELETED"
+
+	for i, v := range id.listUsers {
+		//send message to all users of specified list
+		log.Println("Message sent to ", i, v)
+	}
 }
